666df738a1128e10d4e841024f6cf2c8a916977c
StefanC <Stefan.Cernescu@tss-yonder.com>
2017-03-20 17:46:11 +0200

    Still having problems with sum

--- Sum.py
+++ Sum.py
@@ -7,7 +7,7 @@ class Sum:
         self.val_a = val_a
         self.val_b = val_b
         self.col_a = col_a
-        self.col_b = [int(x) for x in col_b]
+        self.col_b = col_b
         self.b_vector_a = b_vector_a
         self.b_vector_b = b_vector_b
 
@@ -17,59 +17,66 @@ class Sum:
             diagSUM.append(float(self.diag_a[i]) + float(self.diag_b[i]))
         return diagSUM
 
-    def getValuesByLine(self, line, vec):
-        return vec[:vec.index(0)]
-
+    def getValuesByLine(self, vec):
+        element = []
+        nr_elements = len(vec)
+        for i in range(1, nr_elements):
+            if vec[i] != 0:
+                element.append(vec[i])
+            else:
+                return element
+        return []
 
     def getColumsByLine(self, line, vec):
-        if line-1 in vec:
-            return vec[1:vec.index(line-1)]
+        if line - 1 in vec:
+            return vec[1:vec.index(line - 1)]
         return []
+
+    def eliminateValues(self, big_vec, vec):
+        while big_vec[0] == 0:
+            big_vec.remove(0)
+        for x in vec:
+            big_vec.remove(x)
+        return big_vec
 
     def addOthers(self):
         el_final = []
         col_final = []
-        self.val_b.remove(0)
+
         for i in self.col_b:
             if i < 0:
                 el_final.append(0)
                 col_final.append(i)
-                elements_b = self.getValuesByLine(abs(i), self.val_b)
-                self.val_b.remove(0)
-                for el in elements_b:
-                    self.val_b.remove(el)
-                self.val_b.remove(0)
-                print(elements_b)
+                elements_b = self.getValuesByLine(self.val_b)
+                self.val_b = self.eliminateValues(self.val_b, elements_b)
                 columns_b = self.getColumsByLine(i, self.col_b[self.col_b.index(i):])
-                print(columns_b)
-               # elements_a = self.getValuesByLine(abs(i), self.val_a)
-               # columns_a = self.getColumsByLine(i, self.col_a[self.col_a.index(i):])
+                elements_a = self.getValuesByLine(self.val_a)
+                self.val_a = self.eliminateValues(self.val_a, elements_a)
+                columns_a = self.getColumsByLine(i, self.col_a[self.col_a.index(i):])
                 l1 = 0
                 l2 = 0
-        return
-        #         while l1 < len(columns_a) and l2 < len(columns_b):
-        #             if columns_a[l1] > columns_b[l2]:
-        #                 el_final.append(elements_b[l2])
-        #                 col_final.append(columns_b[l2])
-        #                 l2 += 1
-        #             elif columns_a[l1] < columns_b[l2]:
-        #                 el_final.append(elements_a[l1])
-        #                 col_final.append(columns_a[l1])
-        #                 l1 += 1
-        #             else:
-        #                 el_final.append(elements_a[l1] + elements_b[l2])
-        #                 col_final.append(columns_a[l1])
-        #                 l1 += 1
-        #                 l2 += 2
-        #         while l1 < len(columns_a):
-        #             el_final.append(elements_a[l1])
-        #             col_final.append(columns_a[l1])
-        #             l1 += 1
-        #         while l2 < len(columns_b):
-        #             el_final.append(elements_b[l2])
-        #             col_final.append(columns_b[l2])
-        #             l2 += 1
-        #         el_final.append(0)
-        #         col_final.append(i - 1)
-        # print(el_final)
-        # print(col_final)
+                while l1 < len(columns_a) and l2 < len(columns_b):
+                    if columns_a[l1] > columns_b[l2]:
+                        el_final.append(elements_b[l2])
+                        col_final.append(columns_b[l2])
+                        l2 += 1
+                    elif columns_a[l1] < columns_b[l2]:
+                        el_final.append(elements_a[l1])
+                        col_final.append(columns_a[l1])
+                        l1 += 1
+                    else:
+                        el_final.append(elements_a[l1] + elements_b[l2])
+                        col_final.append(columns_a[l1])
+                        l1 += 1
+                        l2 += 2
+                while l1 < len(columns_a):
+                    el_final.append(elements_a[l1])
+                    col_final.append(columns_a[l1])
+                    l1 += 1
+                while l2 < len(columns_b):
+                    el_final.append(elements_b[l2])
+                    col_final.append(columns_b[l2])
+                    l2 += 1
+                el_final.append(0)
+                col_final.append(i - 1)
+        return el_final, col_final
--- main.py
+++ main.py
@@ -25,14 +25,14 @@ class Input:
         val = []
         col = []
         diag = []
-        line=[(float(element.split(", ")[0]), int(element.split(", ")[1]), int(
-                element.split(", ")[2].split("\n")[0])) for element in line]
-        line=sorted(line,key=lambda val:(val[1],val[2]))
+        line = [(float(element.split(", ")[0]), int(element.split(", ")[1]), int(
+            element.split(", ")[2].split("\n")[0])) for element in line]
+        line = sorted(line, key=lambda val: (val[1], val[2]))
         for element in line:
-            value, line_index, column_index = element[0],element[1],element[2]
-            if (-1 * line_index -1) not in col:
+            value, line_index, column_index = element[0], element[1], element[2]
+            if (-1 * line_index - 1) not in col:
                 val.append(0)
-                col.append(-1 * line_index -1)
+                col.append(-1 * line_index - 1)
             if line_index == column_index:
                 diag.append(value)
             else:
@@ -48,17 +48,19 @@ def getInputFromText(filename):
     diag, val, col = x.getOtherElements()
 
     return n, b_vector, diag, val, col
-    return none,none,none,none,none
 
 
 def main():
     n_a, b_vector_a, diag_a, val_a, col_a = getInputFromText("a.txt")
     n_b, b_vector_b, diag_b, val_b, col_b = getInputFromText("b.txt")
-    operation=s.Sum(n_a, n_b, diag_a, diag_b, val_a, val_b, col_a, col_b, b_vector_a, b_vector_b)
+    n_sum, b_vector_sum, diag_sum, val_sum, col_sum = getInputFromText("aplusb.txt")
+    operation = s.Sum(n_a, n_b, diag_a, diag_b, val_a, val_b, col_a, col_b, b_vector_a, b_vector_b)
     # #print (operation.addDiag())
-    start_time=time.time()
-    operation.addOthers()
-    print(abs(time.time()-start_time))
+    start_time = time.time()
+    el_final, col_final = operation.addOthers()
+    print(abs(time.time() - start_time))
+    print(val_sum)
+    print(el_final)
 
 
 main()
